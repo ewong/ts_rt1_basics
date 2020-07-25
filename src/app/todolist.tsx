84c8db3aa17454f6c2568d56556e84536efd466b
Edward Wong <edwardlm.wong@gmail.com>
2020-07-24 21:36:47 -0400

    Working now. But will move add todo to todolist.

--- src/app/todolist.tsx
+++ src/app/todolist.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./todolist.css";
 import { Todo } from "./todo";
+import { AddTodo } from "./addtodo";
 
 interface IProps {}
 
@@ -12,14 +13,21 @@ class TodoList extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-      todos: ["hello", "booyah", "goodbye"],
+      todos: [],
     };
-    this.handleTodo.bind(this);
   }
 
-  handleTodo(i: number) {
+  addTodo(value: string) {
     const todos = this.state.todos.slice();
-    delete todos[i];
+    console.log(todos.length);
+    if (todos.length >= 9) return;
+    todos.push(value);
+    this.setState({ todos });
+  }
+
+  removeTodo(i: number) {
+    const todos = this.state.todos.slice();
+    todos.splice(i, 1);
     this.setState({ todos });
   }
 
@@ -27,13 +35,14 @@ class TodoList extends React.Component<IProps, IState> {
     return (
       <div>
         <div>TodoList</div>
+        <AddTodo onClick={this.addTodo.bind(this)}></AddTodo>
         {this.state.todos.map((value, index) => {
           return (
             <Todo
               key={index}
               value={value}
               onClick={() => {
-                this.handleTodo(index);
+                this.removeTodo(index);
               }}
             ></Todo>
           );
